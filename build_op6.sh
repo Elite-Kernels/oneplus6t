c47f8109ff0973370b3981935375bd9a970447e1
Forrest Marble <forrestmarble@gmail.com>
2019-04-04 11:53:01 -0400

    script: use 8.3 toolchain

--- build_op6.sh
+++ build_op6.sh
@@ -28,7 +28,8 @@
 
 #TOOLCHAIN=/home/forrest/kernel/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 #TOOLCHAIN=/home/forrest/kernel/gcc8.2/bin/aarch64-cortex_a75-linux-android-
-TOOLCHAIN=/home/forrest/kernel/linaro8.2/bin/aarch64-linux-gnu-
+#TOOLCHAIN=/home/forrest/kernel/linaro8.2/bin/aarch64-linux-gnu-
+TOOLCHAIN=/home/forrest/kernel/linaro8.3/bin/aarch64-linux-gnu-
 # export CROSS_COMPILE=/home/forrest/kernel/gcc4.9/bin/aarch64-linux-android-
 export ARCH=arm64
 export SUBARCH=arm64
